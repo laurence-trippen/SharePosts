6272b8ec67a384a0f31efde025e129a306878d42
Laurence Trippen <laurence.trippen@gmail.com>
2020-09-05 22:19:32 +0200

    insert post form added

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -22,11 +22,48 @@ class Posts extends Controller {
   }
 
   public function add() {
-    $data = [
-      'title' => '',
-      'body' => ''
-    ];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // Sanitize POST array
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-    $this->view('posts/add', $data);
+      $data = [
+        'title' => trim($_POST['title']),
+        'body' => trim($_POST['body']),
+        'user_id' => $_SESSION['user_id'],
+        'title_error' => '',
+        'body_error' => ''
+      ];
+
+      // Validate title
+      if (empty($data['title'])) {
+        $data['title_error'] = 'Please enter title';
+      }
+
+      // Validate body
+      if (empty($data['body'])) {
+        $data['body_error'] = 'Please enter body';
+      }
+
+      // Make sure no errors
+      if (!empty($data['title']) && !empty($data['body'])) {
+        // Validateds
+        if ($this->postModel->addPost($data)) {
+          flash('post_added', 'Post Added');
+          redirect('/posts');
+        } else {
+          die('Something went wrong');
+        }
+      } else {
+        // Load view with errors
+        $this->view('posts/add', $data);
+      }
+    } else {
+      $data = [
+        'title' => '',
+        'body' => '',
+      ];
+  
+      $this->view('posts/add', $data);
+    }
   }
 }
